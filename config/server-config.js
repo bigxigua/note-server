903bd87facdc5ab26623d16991c7250878289566
bigxigua <1336945839@qq.com>
2019-05-16 15:31:36 +0800

    set-cookie 修改domain

--- config/server-config.js
+++ config/server-config.js
@@ -1,6 +1,7 @@
 exports.JWT_KEY = 'TBZ_KEY';
+const NODE_ENV = process.env.NODE_ENV;
 exports.cookieConfig = {
-    domain: '127.0.0.1',
+    domain: NODE_ENV === 'production' ? '45.76.98.174' : '127.0.0.1',
     signed: true,
     path: '/',
     maxAge: 60 * 60 * 1000,
