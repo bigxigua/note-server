abb2c3406a854debe997eb04f0ea2a225f2cae43
bigxigua <1336945839@qq.com>
2019-05-17 10:19:49 +0800

    pm2 start 增加环境变量env

--- config/server-config.js
+++ config/server-config.js
@@ -1,5 +1,5 @@
 exports.JWT_KEY = 'TBZ_KEY';
-const NODE_ENV = process.env.NODE_ENV;
+const NODE_ENV = process.env.env;
 exports.cookieConfig = {
     domain: NODE_ENV === 'production' ? '45.76.98.174' : '127.0.0.1',
     signed: true,
