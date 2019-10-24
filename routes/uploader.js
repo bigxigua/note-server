66ea0864857ef7a9f0d315aa759b45a65a360577
bigxigua <1336945839@qq.com>
2019-10-24 15:22:57 +0800

    :fire: 文件上传模块调试

--- routes/uploader.js
+++ routes/uploader.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const { hostname, port } = require('../config/server-config');
 const SERVER_PATH = `${hostname}:${port}/img`;
-const UPLOAD_IMG_PATH = path.join(process.cwd(), '../note-static/img/');
+// const UPLOAD_IMG_PATH = path.join(process.cwd(), '../note-static/img/');
+const UPLOAD_IMG_PATH = path.join(process.cwd(), './pu');
 
 function asyncFileWriteStreamClose(stream, fileName) {
     return new Promise((resolve) => {
@@ -24,15 +25,7 @@ router.post('/uploadImage', async (ctx, next) => {
         const { uuid, fileId } = body;
         const { file } = files;
         const fileReaderStream = fs.createReadStream(file.path);
-        console.log('---UPLOAD_IMG_PATH----', UPLOAD_IMG_PATH);
-        fs.open(UPLOAD_IMG_PATH,'w',function(err,fd){
-            console.log(err, fd, '-----');
-            if (!err) {
-                fs.fstat(fd,function(error,stat){
-                    console.log(error, stat, '---stat--');
-                })
-            }
-        });
+        console.log('---UPLOAD_IMG_PATH----', UPLOAD_IMG_PATH, file.path);
         const fileWriteStream = fs.createWriteStream(UPLOAD_IMG_PATH);
         fileReaderStream.pipe(fileWriteStream);
         const sign = await asyncFileWriteStreamClose(fileWriteStream, file.name);
