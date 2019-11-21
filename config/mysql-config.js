0a4689f23bf6bad27b46b960bf43cc770ad9fafe
bigxigua <1336945839@qq.com>
2019-11-21 12:19:13 +0800

    :construction: recover port

--- config/mysql-config.js
+++ config/mysql-config.js
@@ -2,7 +2,7 @@ const NODE_ENV = process.env.env;
 module.exports = {
   host: NODE_ENV === 'production' ? '139.196.84.53' : '127.0.0.1',
   port: 3306,
-  user: NODE_ENV === 'production' ? 'data' : 'root',
+  user: 'root',
   password: NODE_ENV === 'production' ? 'shuai' : 'shuaitbz', // tbzshuai
   database: 'note_db',
   connectionLimit: 10,
