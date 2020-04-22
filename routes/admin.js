7e981496ac0fd2ed80e6e05985a7fe7b036e1b8b
bigxigua <1336945839@qq.com>
2020-04-22 09:43:15 +0800

    bugfix: 修复json parse报错

--- routes/admin.js
+++ routes/admin.js
@@ -7,9 +7,9 @@ router.post('/api/ip', async (ctx) => {
   const { body: { ip } } = ctx.request;
   const configFileName = path.join(__dirname, '../upload') + `/config.json`;
   const configJson = await fs.readFileSync(configFileName, { encoding: 'utf-8' });
-  const config = JSON.parse(configJson);
+  const config = JSON.parse(configJson || '{}');
   const newIps = ip.split(',').filter(n => n);
-  config.ips = [...new Set([].concat(config.ips, newIps))];
+  config.ips = [...new Set([].concat((config.ips || []), newIps))];
   await fs.writeFileSync(configFileName, JSON.stringify(config));
   ctx.body = serializReuslt('SUCCESS');
 });
@@ -18,7 +18,7 @@ router.get('/api/ips', async (ctx) => {
   const { body: { ip } } = ctx.request;
   const configFileName = path.join(__dirname, '../upload') + `/config.json`;
   const configJson = await fs.readFileSync(configFileName, { encoding: 'utf-8' });
-  const ips = JSON.parse(configJson).ips;
+  const ips = JSON.parse(configJson || '{}').ips || [];
   ctx.body = serializReuslt('SUCCESS', { ips });
 });
 
